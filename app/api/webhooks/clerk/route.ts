5b65f8d29676c7ceb17ae745565c84ff9bd87f80
Anurag Raj <adityanv8@gmail.com>
2025-10-09 01:56:55 +0530

    completed with signup and signin

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,20 +1,62 @@
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextRequest } from 'next/server'
-
+import { db } from '@/lib/db'
 export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req)
 
     // Do something with payload
     // For this guide, log payload to console
-    const { id } = evt.data
+    const { id,username,image_url } = evt.data
     const eventType = evt.type
-    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
-    console.log('Webhook payload:', evt.data)
+    if (eventType === 'user.created') {
+      await db.user.create({
+        data:{
+          externalUserId:id!,
+          username:username,
+          imageUrl:image_url
 
-    return new Response('Webhook received', { status: 200 })
+        }
+      })
+
+      return new Response('User Created Succesfully',{status:200})
+    }
+
+    if (eventType === 'user.updated') {
+      const currentUser = await db.user.findUnique({
+        where:{
+          externalUserId:id
+        }
+      })
+
+      if (!currentUser) {
+        return new Response("User not found",{status:404})
+      }
+
+      await db.user.update({
+        where:{
+          externalUserId:id
+        },
+        data:{
+          username:username,
+          imageUrl:image_url          
+        }
+      })
+      return new Response('User Updated Succesfully',{status:200})
+    }
+
+    if (eventType=='user.deleted') {
+      await db.user.delete({
+        where:{
+          externalUserId:id
+        }
+      })
+      return new Response('User Deleted Succesfully',{status:200})
+    }
   } catch (err) {
-    console.error('Error verifying webhook:', err)
-    return new Response('Error verifying webhook', { status: 400 })
+    return Response.json({
+      message:"User Not Created",
+      error:err
+    })
   }
 }
