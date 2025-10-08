6887a883325c9b62d4f703e5a48fab5f03cfe2ce
Anurag Raj <adityanv8@gmail.com>
2025-10-08 11:06:34 +0530

    first commit

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,18 @@
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+
+
+const font = Poppins({
+  subsets:["latin"],
+  weight:['200','300','400','500','600','700','800']
+})
+
+export const Logo = ()=>{
+  return <div className=" flex flex-col items-center gap-y-4">
+    <div className="rounded-full p-1 flex justify-center gap-y-2 items-center flex-col">
+      <Image src='/logo.svg' alt="Logo" width={80} height={80}>
+      </Image>
+      <div className=" text-3xl font-semibold ">EDUNAX GAMEHUB </div>
+    </div>
+  </div>
+}
